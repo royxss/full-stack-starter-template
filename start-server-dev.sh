159861769111d1f2410b106e9a67c50a45d9977e
royxss <findroy@outlook.in>
2020-09-02 14:32:03 -0500

    Added checks

--- start-server-dev.sh
+++ start-server-dev.sh
@@ -2,4 +2,10 @@
 #export DJANGO_SETTINGS_MODULE="backend.settings.development"
 cd backend/
 pytest -v
-python manage.py runserver
+pytest -v > unit-test.log
+if grep -q failed unit-test.log
+then
+    echo "Test cases have failed. Server will not start..."
+else
+    python manage.py runserver
+fi
